2ec2c5538d3d233dd4eddf1b2960ce1d7ea770ab
Slav Donchev <srd@Slavs-MacBook-Pro.local>
2018-12-05 17:12:45 +0200

    add vagrant

---
+++ vagrant/scripts/provision.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# Provision with the needed packages
+
+# register the Microsoft key and the product repository
+wget -P /tmp -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
+sudo dpkg -i /tmp/packages-microsoft-prod.deb
+
+# install needed packages
+sudo apt-get update
+sudo apt-get install -y apt-transport-https dotnet-sdk-2.1 git azure-functions-core-tools curl git
+
+# clone the GitHub repo in vagrant user home directory
+git clone https://github.com/slavrd/az-hello-dfunc.git
+sudo chown -R vagrant /home/vagrant/az-hello-dfunc
+
+# clean up
+sudo apt-get clean
+rm /tmp/packages-microsoft-prod.deb
+
